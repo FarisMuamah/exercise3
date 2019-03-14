4aba86856f599339e33037e816100b176eb81835
faris muamah <44923140+FarisMuamah@users.noreply.github.com>
2019-03-13 13:57:59 -1200

    Add files via upload

--- exercise3.js
+++ exercise3.js
@@ -240,6 +240,24 @@ for(var i = 0;i<data.results.length; i++) {
 
 }
 
-function submitsearch () {
-    var search = document
-}
+// function submitsearch () {
+//     var search = document
+// }
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("content");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
